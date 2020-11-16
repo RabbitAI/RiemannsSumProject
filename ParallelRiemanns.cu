eec2a249cbd02ed7e26d8deaeff29ca0100ac00d
Christian Douglas <cedoug13@aol.com>
2020-11-15 23:41:40 -0600

    finish?

--- ParallelRiemanns.cu
+++ ParallelRiemanns.cu
@@ -5,44 +5,74 @@
 //Parallel code
 
 #include <stdio.h>
+#include "timer.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 #define NUMBER_OF_THREADS 1024
 
 // CUDA kernel
-__global__ void findArea(float* deltaX)
-{   __shared__float sum = 0;
-
-    // Get our global thread ID
-    float id = blockIdx.x*blockDim.x+threadIdx.x;
-    if (id == 0 || id == NUMBER_OF_THREADS) {
-        sum += (id * id) + (id * 3) + 3;
-    }  else {
-        float x = (id) * deltaX;
-        sum += 2 * ((x * x) + (x * 3) + 3);
+__global__ void findArea(float *n, float a, float b)
+{	__shared__ float dX;
+	dX = (b - a)/NUMBER_OF_THREADS;
+    // Get our thread ID
+    int id = blockIdx.x*blockDim.x+threadIdx.x;
+	
+    if (id == 0 ) {
+        n[id] = (a * a) + (a * 3) + 3;
+	}
+	if (id == NUMBER_OF_THREADS-1){
+		n[id] = (b * b) + (b * 3) + 3;
+    } else if(id != 0) {
+		float x = id * dX;
+		n[id] = (2 * ((x * x) + (x * 3) + 3));
     }
 }
 
 int main()
-{
+{	
+    //timer variables
+    double start, finish, elapsed;
     //host variables
-    float a = 0, b = 100,
-     deltaX = (b-a)/NUMBER_OF_THREADS;
+	float a = 0, b = 100;
+	float sum = 0;
+	const int size = NUMBER_OF_THREADS*sizeof(float);
+    float n[size];
+	
+	float total = 0;
     //device variables
-    float* deltaX_dev;
+	float* n_dev;
 
     //allocate on gpu 
-    cudaMalloc((void**)a_dev, deltaX);
-    cudaMemcpy(deltaX_dev, deltaX, deltaX, cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&n_dev, size);
+    cudaMemcpy(n_dev, n, size, cudaMemcpyHostToDevice);
 
     dim3 GridDim(1,1);
     dim3 BlockDim(NUMBER_OF_THREADS,1);
 
+    GET_TIME(start);
     //call kernel 
-    findArea<<<GridDim,BlockDim>>>(deltaX_dev);
+    findArea<<<GridDim,BlockDim>>>(n_dev, a, b);
+    cudaDeviceSynchronize();
+    GET_TIME(finish);
+    elapsed = finish - start;
+
+	cudaMemcpy(n, n_dev, size, cudaMemcpyDeviceToHost);
+    cudaFree(n_dev);
     
-    cudaFree(deltaX_dev);
+	for (int i = 0; i < NUMBER_OF_THREADS; i++) 
+	{	
+        sum += n[i];
+	}
     
-    printf("Riemann's Sum: %f\n", sum);
+	total = ((b - a)/(2 * NUMBER_OF_THREADS)) * sum;
+	
+    printf("Riemann's Sum: %f\n", total);
+    printf("The code to be timed took %e seconds\n", elapsed);
 
     return 0;
 }
+
+//shared memory will be deltaX
+//pass in array = #threads for holding y values
+//AVOID HEADACHE
--- SerialRiemanns.cu
+++ SerialRiemanns.cu
@@ -5,15 +5,20 @@
 //Serial Code
 
 #include <stdio.h>
+#include "timer.h"
 
 int main()
 {
+    //timer variables
+    double start, finish, elapsed;
+
     float y;
     float a = 0;   //start
     float b = 100; //end
     float n = 1024; //number of intervals
     float deltaX = (b - a) / n, sum = 0;
 
+    GET_TIME(start);
     for (float i = 0; i < n; i++)
     {   if (i == 0)
         {  y = (a * a) + (a * 3) + 3;
@@ -29,10 +34,12 @@ int main()
             sum += y;
         } 
     }
-
+    GET_TIME(finish);
+    elapsed = finish - start;
     sum = (deltaX / 2) * sum;
 
     printf("The Riemann's Sum is: %f\n", sum);
-
+    printf("The code to be timed took %e seconds\n", elapsed);
+    
     return 0;
 }
